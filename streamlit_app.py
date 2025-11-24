2efe0c87be154ed5f17cb22619ae090c6f29fee9
SafyaDevv <44072430+SafyaDevv@users.noreply.github.com>
2025-11-24 13:36:22 +0000

    feat: trying out expander and toggles in streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import data_exploration as data_exp
 
-st.title("Caviardage")
+st.title("Caviardage 📜🐙")
+
+data = data_exp.dataFrame
+
 st.write(
-    "Woop woop"
+    "Woop woop",
 )
+
+#TRYING OUT TOGGLES
+#displayDataFrame = st.toggle("Display full original dataset")
+
+# if displayDataFrame:
+#     st.subheader("The original Blackout dataset")
+#     st.dataframe(data, use_container_width=True)
+
+
+expand = st.expander("The original Blackout dataset", icon=":material/info:")
+expand.dataframe(data, use_container_width=True)
+
