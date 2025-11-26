9055b9d70b651fec07436f25f47f6c3d77f629d1
SafyaDevv <safya.wane@gmail.com>
2025-11-26 23:44:03 +0000

    feat: started data cleaning

--- data_exploration.py
+++ data_exploration.py
@@ -1,11 +1,27 @@
 import pandas
+import numpy
 import json
 
-file = "files\data_16k.json"
+file = "files/data_16k.json"
 
 dataFrame = pandas.read_json(file)
 
-print(dataFrame.head())
+print("Infos before cleaning non alphanumerical:")
+dataFrame.info()
+
+#print(dataFrame.head())
 
 # dataFrame.to_csv("test.csv", index=False)
+
+# ************** DATA CLEANING **************
+
+dataFrame["poem"] = dataFrame["poem"].str.strip() #removing leading/trailing spaces
+
+#remove rows with strings in poem column that contain non-letters (aside from spaces)
+dataFrame = dataFrame[~dataFrame["poem"].str.contains(r"[^a-zA-Z\s]", na=False)]
+
+#ensure no poems have randoms single letters (aside from a, i, o since those are words)
+dataFrame = dataFrame[~dataFrame["poem"].str.contains(r"\b(?![aio]\b)[a-z]\b", 
+                                                      na=False, case=False)]
+dataFrame.info()
 
