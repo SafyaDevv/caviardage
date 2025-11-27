a2c427883f21ad3479ff24ead6ab028a6a641a40
SafyaDevv <safya.wane@gmail.com>
2025-11-27 00:21:50 +0000

    feat: finished cleaning data, removed poems with numbers and random characters

--- data_exploration.py
+++ data_exploration.py
@@ -1,27 +1,40 @@
 import pandas
 import numpy
 import json
+import matplotlib.pyplot as plot
+import numpy
 
 file = "files/data_16k.json"
 
 dataFrame = pandas.read_json(file)
 
-print("Infos before cleaning non alphanumerical:")
+print("Infos before cleaning up:")
 dataFrame.info()
-
-#print(dataFrame.head())
 
 # dataFrame.to_csv("test.csv", index=False)
 
 # ************** DATA CLEANING **************
 
-dataFrame["poem"] = dataFrame["poem"].str.strip() #removing leading/trailing spaces
+alphaPoems = dataFrame["poem"].str.contains(r"[^a-zA-Z\s]", na=False) 
+cleanDf = dataFrame[~alphaPoems] #remove rows with strings in poem column that contain non-letters (aside from spaces)
 
-#remove rows with strings in poem column that contain non-letters (aside from spaces)
-dataFrame = dataFrame[~dataFrame["poem"].str.contains(r"[^a-zA-Z\s]", na=False)]
+print("count of alpha poems: ", len(alphaPoems))
 
-#ensure no poems have randoms single letters (aside from a, i, o since those are words)
-dataFrame = dataFrame[~dataFrame["poem"].str.contains(r"\b(?![aio]\b)[a-z]\b", 
-                                                      na=False, case=False)]
-dataFrame.info()
+cleanDf = cleanDf[~cleanDf["poem"].str.contains(r"\b(?![aio]\b)[a-z]\b", 
+                                                      na=False, case=False)] #ensure no poems have randoms single letters (aside from a, i, o since those are words)
+#count of poems with random single letters
+#INSERT
+
+cleanDf.drop_duplicates(subset=["poem"], inplace=True, keep="first") #when there is duplicated poems, only keep one of them
+#count of poems that were duplicates
+#INSERT
+
+#Pie chart showing categories of poems removed
+#INSERT
+
+
+cleanDf.info()
+
+cleanDf["poem"] = cleanDf["poem"].str.strip() #removing leading/trailing spaces
+
 
