4e976d629f692df3f70a5d36fc6d8026a226da67
SafyaDevv <safya.wane@gmail.com>
2025-11-28 18:32:57 +0000

    feat: added function to filter out unrecognised POS

--- generate_dataset.py
+++ generate_dataset.py
@@ -4,13 +4,22 @@
 ## to avoid re-computing POS tagging each time those scripts are run
 
 import pandas
-from pos_handler import apply_pos
+from pos_handler import apply_pos, check_for_unknown_pos
 from data_pipeline import clean_df
 
 clean_df = clean_df.drop(columns=["grammar-check"]) #Dropping grammar-check column as all values are false
 poem_index = clean_df.columns.get_loc("poem")
 
 pos_tags = clean_df["poem"].apply(apply_pos) #generating POS tags for each poem
+
+#removing poems with words that are unrecognised by spaCy small english model (not in english or gibberish)
+unknown_pos = []
+for tag in pos_tags:
+    if(check_for_unknown_pos(tag)):
+        unknown_pos.append(tag)
+
+
+print("Poems with unknown POS tags (X): ", len(unknown_pos))
 
 clean_df.insert(poem_index + 1, "part-of-speech", pos_tags) #inserting new column with POS tags next to poem column
 
--- pos_handler.py
+++ pos_handler.py
@@ -14,13 +14,16 @@ def apply_pos(poem):
     
     return pos_tags
 
+#return True if any unknown POS tags (X) are found in the list of tags
+#used in generate_dataset.py to filter out poems with unrecognised words
+def check_for_unknown_pos(tag):
+    if any(t == "X" for t in tag):
+        return True
+    return False
+
 #function to find poems marked as having good grammar in Blackout dataset
 #meaning grammar-check column is True
 # /!\ WILL USE LATER FOR ANALYSIS LATER, why are those marked as true?/!\
 def get_grammar_check(df):
     df = df[df["grammar-check"]]
     return df["poem"]
-
-
-
-
