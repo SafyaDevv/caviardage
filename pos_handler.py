b65812a08485b2b02f44b45c63f4c1b95e14e64d
SafyaDevv <safya.wane@gmail.com>
2025-12-06 19:45:12 +0000

    feat: switched to large spacy model

--- pos_handler.py
+++ pos_handler.py
@@ -2,9 +2,8 @@
 ### and handling grammar check & POS-related tasks
 
 import spacy
-#from data_pipeline import blackout_df
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_lg")
 
 # **** TOKENISATION AND PART-OF-SPEECH TAGGING ****
 
@@ -18,16 +17,3 @@ def apply_pos(poem):
 #used in generate_dataset.py to filter out poems with unrecognised words
 def has_unknown_pos(tags):
     return any(t == "X" for t in tags)
-
-#function to find poems marked as having good grammar in Blackout dataset
-#meaning grammar-check column is True
-# /!\ WILL USE LATER FOR ANALYSIS LATER, why are those marked as true?/!\
-def get_grammar_check(df):
-    df = df[df["grammar-check"]]
-    return df["poem"]
-
-#NOTE: THE GOAL HEAR WAS TO CHECK THE POS OF POEMS MARKED AS GOOD GRAMMAR
-#BUT IT's NOT TOO IMPORTANT SO I'M LEAVING IT FOR NOW !!!!
-# good_grammar_poems = blackout_df[get_grammar_check(blackout_df)]
-# tags = good_grammar_poems["poem"].apply(apply_pos)
-# print(tags)
